8b84ded3a9052921f33abad8d30d6317d1b92274
LucasManera2017 <lucas.manera2017@gmail.com>
2026-01-09 15:24:46 -0300

    fixed

--- app.config.ts
+++ app.config.ts
@@ -4,8 +4,50 @@ export default {
   expo: {
     name: "HabitFlow",
     slug: "habitflow",
+    version: "1.0.0",
+
+    icon: "./assets/images/icon.png",
+    scheme: "habitflow",
+    orientation: "portrait",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+
+    ios: {
+      supportsTablet: true,
+    },
+
+    android: {
+      package: "com.lucasmanera.habitflow",
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundColor: "#E6F4FE",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+    },
+
+    plugins: [
+      "expo-router",
+      "expo-font",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+    ],
+
     extra: {
       apiUrl: process.env.EXPO_PUBLIC_API_URL,
+
+      eas: {
+        projectId: "17b262fc-9bf3-48ac-83d9-6ca7b160e3ab",
+      },
     },
   },
 };
